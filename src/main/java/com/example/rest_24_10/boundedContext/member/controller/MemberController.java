9592312cb720f42b71bf9911145278c87a863dda
woooyoung <axdsw121@gmail.com>
2024-10-31 13:25:39 +0900

    RsData와 LoginResponse 클래스, 규격화 더 쉽게

--- src/main/java/com/example/rest_24_10/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/example/rest_24_10/boundedContext/member/controller/MemberController.java
@@ -1,10 +1,13 @@
 package com.example.rest_24_10.boundedContext.member.controller;
 
+import com.example.rest_24_10.base.rsData.RsData;
 import com.example.rest_24_10.boundedContext.member.service.MemberService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,21 +31,19 @@ public class MemberController {
         private String password;
     }
 
+    @AllArgsConstructor
+    @Getter
+    public static class LoginResponse {
+        private final String accessToken;
+    }
+
     @PostMapping("/login")
-    public String login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
+    public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
         String accessToken = memberService.genAccessToken(loginRequest.getUsername(), loginRequest.getPassword());
 
         resp.addHeader("Authentication", accessToken);
 
-        return """
-                {
-                "resultCode" : "S-1",
-                "msg" : "액세스 토큰이 생성되었습니다.",
-                "data" : {
-                    "Access Token: " + "%s"
-                    }
-                }
-                """.formatted(accessToken).stripIndent();
+        return RsData.of("S-1", "액세스 토큰 생성됨", new LoginResponse(accessToken));
     }
 
 }
