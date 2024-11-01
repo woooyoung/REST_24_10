3d97528d0534cd0ef638c63511cd3119b643e6a4
woooyoung <axdsw121@gmail.com>
2024-11-01 14:46:24 +0900

    스웨거에서 액세스 토큰 설정할 수 있도록

--- src/main/java/com/example/rest_24_10/boundedContext/member/controller/Api1MemberController.java
+++ src/main/java/com/example/rest_24_10/boundedContext/member/controller/Api1MemberController.java
@@ -4,6 +4,8 @@ import com.example.rest_24_10.base.rsData.RsData;
 import com.example.rest_24_10.boundedContext.member.dto.MemberDto;
 import com.example.rest_24_10.boundedContext.member.entity.Member;
 import com.example.rest_24_10.boundedContext.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -54,6 +56,7 @@ public class Api1MemberController {
 
     //consumes = ALL_VALUE -> Json 형태로 입력받는게 필수가 아니다.
     @GetMapping(value = "/me", consumes = ALL_VALUE)
+    @Operation(summary = "로그인 된 사용자 정보", security = @SecurityRequirement(name = "bearerAuth"))
     public RsData<MeResponse> me(@AuthenticationPrincipal User user) {
         Member member = memberService.findByUsername(user.getUsername()).get();
 
